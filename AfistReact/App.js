853f8b19d2b5ceb067b733fddcbe0b412e052c8f
Furkan Ozoglu <furkan.ozoglu@ibb.gov.tr>
2018-12-11 16:10:32 +0300

    added Furkan page

--- AfistReact/App.js
+++ AfistReact/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
@@ -31,8 +31,22 @@ class AkifeScreen extends React.Component {
 class FurkanScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Furkan</Text>
+      <View style={stylesFurkan.container}>
+          <View style={stylesFurkan.header}></View>
+          <Image style={stylesFurkan.avatar} source={{uri: 'https://avatars0.githubusercontent.com/u/9076939?s=460&v=4'}}/>
+          <View style={stylesFurkan.body}>
+            <View style={stylesFurkan.bodyContent}>
+              <Text style={stylesFurkan.name2}>Furkan Özoğlu</Text>
+              <Text style={stylesFurkan.info}>UX Designer / iOS Mobile developer</Text>
+              <Text style={stylesFurkan.description}>Merhaba arkadaşlar, ilk react sayfamı hazırladım. Umarım Beğenirsiniz.</Text>
+              <TouchableOpacity style={stylesFurkan.buttonContainer}>
+                <Text style={stylesFurkan.buttonText}>CV</Text>  
+              </TouchableOpacity>              
+              <TouchableOpacity style={stylesFurkan.buttonContainer}>
+                <Text style={stylesFurkan.buttonText}>About</Text> 
+              </TouchableOpacity>
+            </View>
+        </View>
       </View>
     );
   }
@@ -84,7 +98,13 @@ const TabNavigator = createBottomTabNavigator({
       if (routeName === 'Akife') {
         iconName = `ios-information-circle${focused ? '' : '-outline'}`;
       } else if (routeName === 'Furkan') {
-        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        if(focused) {
+          iconName = `ios-battery-full`;
+
+        }
+        else {
+          iconName = `ios-battery-dead`;
+        }
       }
       else if (routeName === 'Ikbal') {
         if(focused) {
@@ -109,7 +129,7 @@ const TabNavigator = createBottomTabNavigator({
     },
   }),
   tabBarOptions: {
-    activeTintColor: 'blue',
+    activeTintColor: '#00BFFF',
     inactiveTintColor: 'gray',
   },
 }
@@ -117,3 +137,65 @@ const TabNavigator = createBottomTabNavigator({
 
 export default createAppContainer(TabNavigator);
 
+const stylesFurkan = StyleSheet.create({
+  header:{
+    backgroundColor: "#00BFFF",
+    height:200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600'
+  },
+  body:{
+    marginTop:40,
+  },
+  bodyContent: {
+    alignItems: 'center',
+    padding:40,
+  },
+  name2:{
+    fontSize:28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  info:{
+    fontSize:16,
+    color: "#00BFFF",
+    marginTop:10
+  },
+  description:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center',
+    marginBottom: 40
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00BFFF",
+  },
+  buttonText:{
+    fontSize:18,
+    color: "white",
+    fontWeight: "600"
+  },
+});
